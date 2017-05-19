80af65a7b1d81ef70d42a60c9377495c305ae67c
Brad <bradley.wood@uoit.net>
2017-05-18 21:16:27 -0400

    Skeleton

---
+++ src/main/java/localization/processor/DefaultFaceLocalizer.java
@@ -0,0 +1,19 @@
+package localization.processor;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+/**
+ * Created by Brad on 5/18/2017.
+ */
+public class DefaultFaceLocalizer extends Localizer {
+
+    public void update(BufferedImage image) {
+
+    }
+    public List<Rectangle> localize() {
+        return null;
+    }
+
+}
---
+++ src/main/java/localization/processor/Localizer.java
@@ -0,0 +1,16 @@
+package localization.processor;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+/**
+ * Created by Brad on 5/18/2017.
+ */
+public abstract class Localizer {
+
+    public abstract void update(BufferedImage image);
+
+    public abstract List<Rectangle> localize();
+
+}
